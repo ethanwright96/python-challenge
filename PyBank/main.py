95af031141404533e5eaf481c6aa64262fc1c9e5
Ethan Wright <ethan_wright@alumni.brown.edu>
2023-03-21 13:52:41 -0400

    Backup after minor edits

--- PyBank/main.py
+++ PyBank/main.py
@@ -64,25 +64,3 @@ with open (os.path.join("analysis","financial_analysis.txt"), 'w') as file:
     file.close()
     pass
 
-# Your task is to create a Python script that analyzes the records to calculate each of the following values:
-
-# * The total number of months included in the dataset
-
-# * The net total amount of "Profit/Losses" over the entire period
-
-# * The changes in "Profit/Losses" over the entire period, and then the average of those changes
-
-# * The greatest increase in profits (date and amount) over the entire period
-
-# * The greatest decrease in profits (date and amount) over the entire period
-
-# Your analysis should align with the following results:
-
-
-# Financial Analysis
-# ----------------------------
-# Total Months: 86
-# Total: $22564198
-# Average Change: $-8311.11
-# Greatest Increase in Profits: Aug-16 ($1862002)
-# Greatest Decrease in Profits: Feb-14 ($-1825558)
